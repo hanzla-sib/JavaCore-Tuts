d7c21dcc96d8979db03407c2c74d3277e381a85c
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-10 00:20:33 +0100

    isPrime

---
+++ primeNumber.js
@@ -0,0 +1,11 @@
+const isPrime = (num) => {
+  for (var i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true
+};
+
+console.log(isPrime(7));
+console.log(isPrime(10));
