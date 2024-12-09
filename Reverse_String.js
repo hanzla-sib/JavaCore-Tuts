78431e4aaeaa3149029a013d375d5c1661368229
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-09 23:31:28 +0100

    practice

---
+++ Reverse_String.js
@@ -0,0 +1,12 @@
+const Reverse=(str)=>{
+    let reversedStr='';
+    for (let i=str.length-1;i>=0;i--){
+        reversedStr+=str[i];
+    }
+
+    return reversedStr;
+
+}
+
+
+console.log(Reverse('Hello World')) 
