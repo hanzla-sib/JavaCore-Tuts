0803583f4efbe93c651eb8e31e2f0db395059c5c
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-09 23:57:09 +0100

    removeDuplicates

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,2 +1,13 @@
 
-removeDuplciate();
+const removeDuplciate=(arr)=>{
+const dupArr=[];
+for(let i of arr){
+    if(!dupArr.includes(i)){
+        dupArr.push(i)
+    }
+}
+return dupArr
+}
+
+
+console.log(removeDuplciate([1,2,3,4,4,5,6,7,7,8]));
