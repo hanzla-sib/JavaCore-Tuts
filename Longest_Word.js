e9c8610df4e93f9101964d7aa8b05fbbbfb356fa
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-09 23:41:47 +0100

    LongestWord

--- Longest_Word.js
+++ Longest_Word.js
@@ -1,3 +1,15 @@
 const findLongestword = (str) => {
-    
+    let splittedWords=str.split(' ');
+    let longestword="";
+    console.log(splittedWords);
+   
+    splittedWords.forEach(element => {
+        if(element.length>longestword.length){
+            longestword=element;
+        }
+    });
+
+    return longestword;
 }
+
+console.log(findLongestword('Theasdasdasdas quick brown fox jumped over the lazy dog')); //jumped
