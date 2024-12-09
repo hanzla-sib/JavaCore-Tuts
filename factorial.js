8a986b2deac9c7933cbfe9ee6f49b01d167160c8
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-10 00:24:44 +0100

    fact efficcieny

--- factorial.js
+++ factorial.js
@@ -1,9 +1,12 @@
 function Factorial(num) {
+  if (num == 0) {
+    return 1;
+  }
   var fact = 1;
   for (var i = 1; i <= num; i++) {
     fact = fact * i;
   }
-  return fact
+  return fact;
 }
 
 console.log(Factorial(10));
